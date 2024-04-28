2273dc2f322865ca4da136a72032c148ac0a17bf
draganakatic <draganak3108@gmail.com>
2024-04-28 23:39:31 +0200

    pomoziBoze

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.storage.ktx)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/bakavisnja/NoviReceptActivity.kt
+++ app/src/main/java/com/example/bakavisnja/NoviReceptActivity.kt
@@ -1,29 +1,91 @@
 package com.example.bakavisnja
 
+import android.app.ProgressDialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bakavisnja.databinding.ActivityKuhinjaBinding
+import com.example.bakavisnja.databinding.ActivityNoviReceptBinding
+import com.google.android.gms.auth.api.signin.internal.Storage
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.util.UUID
 
 class NoviReceptActivity : AppCompatActivity() {
 
     private lateinit var btnDodaj: Button
+    private lateinit var binding: ActivityNoviReceptBinding
+    private lateinit var mAuth : FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var btnSlika: Button
+    private lateinit var btnDodajSliku: Button
+    private lateinit var imSlika: ImageView
+    private lateinit var selektovanaKategorija: String
+    private lateinit var selektovanoVreme: String
+    private lateinit var imgURL: String
+    var fileUri: Uri? = null
+
     val kategorije = arrayOf("слатко", "слано")
     val vreme = arrayOf("15-30 минута", "30-60 минута", "више од сат времена")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityNoviReceptBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_novi_recept)
+        setContentView(binding.root)
 
         val spinner = findViewById<Spinner>(R.id.spinnerK)
+        mAuth = FirebaseAuth.getInstance()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Молимо Вас сачекајте!")
+        progressDialog.setCanceledOnTouchOutside(false)
+        btnSlika = findViewById(R.id.kamerica)
+        btnDodajSliku = findViewById(R.id.dodaj_sliku)
+        imSlika = findViewById(R.id.slicica)
+
+        btnSlika.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+
+            startActivityForResult(
+                Intent.createChooser(intent,"Изаберите слику"),0
+            )
+
+ //           }
+        }
+        btnDodajSliku.setOnClickListener {
+            if(fileUri != null){
+               uploadImage()
+                imgURL = fileUri.toString()
+               }
+           else{
+               Toast.makeText(applicationContext,"Одаберите слику",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.novirecept.setOnClickListener {
+
+            validateData()
+        }
+
         val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,kategorije)
         spinner.adapter = arrayAdapter
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
@@ -34,7 +96,7 @@ class NoviReceptActivity : AppCompatActivity() {
                 id: Long
             ) {
                 Toast.makeText(applicationContext, "Одабрана категорија је "+ kategorije[position], Toast.LENGTH_SHORT).show()
-
+                selektovanaKategorija = spinner.getSelectedItem().toString()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -55,6 +117,7 @@ class NoviReceptActivity : AppCompatActivity() {
                 id: Long
             ) {
                 Toast.makeText(applicationContext, "Одабрано време је "+ vreme[position], Toast.LENGTH_SHORT).show()
+                selektovanoVreme = spinnerV.getSelectedItem().toString()
 
             }
 
@@ -64,17 +127,95 @@ class NoviReceptActivity : AppCompatActivity() {
 
         }
 
-
-        btnDodaj = findViewById(R.id.novirecept)
-
-        btnDodaj.setOnClickListener{
-            val intent = Intent(this, KuhinjaActivity::class.java)
-            startActivity(intent)
-        }
+//
+//        btnDodaj = findViewById(R.id.novirecept)
+//
+//        btnDodaj.setOnClickListener{
+//            val intent = Intent(this, KuhinjaActivity::class.java)
+//            startActivity(intent)
+//        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 0 && resultCode == RESULT_OK && data != null && data.data != null){
+            fileUri = data.data
+            try {
+                val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(contentResolver,fileUri)
+                imSlika.setImageBitmap(bitmap)
+            }catch (e: Exception){
+                Log.e("Exception" ,"Error" + e)
+            }
+        }
+    }
+    fun uploadImage(){
+        if(fileUri != null){
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Учитавање...")
+            progressDialog.setMessage("Сачекајте...")
+            progressDialog.show()
+
+            val ref: StorageReference = FirebaseStorage.getInstance().getReference().child(UUID.randomUUID().toString())
+            ref.putFile(fileUri!!).addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext,"Слика је успешно учитана!",Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                progressDialog.dismiss()
+                Toast.makeText(applicationContext,"Слика је неуспешно учитана!",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private var imerecepta = ""
+    private var sastojci = ""
+    private var koraci = ""
+
+    private fun validateData() {
+        imerecepta = binding.ime2.text.toString().trim()
+        sastojci = binding.sastojci.text.toString().trim()
+        koraci = binding.koraci.text.toString().trim()
+
+        if(imerecepta.isEmpty() || sastojci.isEmpty() || koraci.isEmpty()){
+            Toast.makeText(this,"Молимо Вас, попуните сва поља!",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            addReceptFirebase()
+        }
+    }
+
+
+    private fun addReceptFirebase() {
+        progressDialog.show()
+
+        val timestamp = System.currentTimeMillis()
+        val hashMap = HashMap<String,Any>()
+        hashMap["id"] = "$timestamp"
+        hashMap["naziv"] = imerecepta
+        hashMap["sastojci"] = sastojci
+        hashMap["koraci"] = koraci
+        hashMap["kategorija"] = selektovanaKategorija
+        hashMap["vreme"] = selektovanoVreme
+        hashMap["slika"] = imgURL
+
+        hashMap["timestamp"] = timestamp
+        hashMap["uid"] = "${mAuth.uid}"
+        val ref = FirebaseDatabase.getInstance().getReference("Recept")
+        ref.child("$timestamp")
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(this,"Успешно сте додали рецепт у кухињу!",Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(this,"Рецепт не може да се дода у кухињу",Toast.LENGTH_SHORT).show()
+            }
+    }
 }
+
+
